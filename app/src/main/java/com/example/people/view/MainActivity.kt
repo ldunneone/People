0ec804bd1bd9af92be99d7c864842172721dc70c
ldunneone <ldunne.one@live.com>
2022-02-13 18:44:51 +0000

    Added the required dependencies

--- app/src/main/java/com/example/people/view/MainActivity.kt
+++ app/src/main/java/com/example/people/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.people
+package com.example.people.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.people.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
