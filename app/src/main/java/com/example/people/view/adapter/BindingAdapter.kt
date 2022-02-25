81a3a64486109da50626ba52b1e3183ffe74aaf2
ldunneone <ldunne.one@live.com>
2022-02-25 16:18:35 +0000

    Colour changing with databinding

--- app/src/main/java/com/example/people/view/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/people/view/adapter/BindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.people.view.adapter
 
+import android.graphics.Color
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
@@ -10,4 +12,9 @@ fun loadBadge(imageView: ImageView, url: String?){
     Glide.with(imageView.context)
         .load(url)
         .into(imageView)
+}
+
+@BindingAdapter("setBackgroundColor")
+fun setBackgroundColor(textView: TextView, favouriteColor: String){
+    textView.setBackgroundColor(Color.parseColor(favouriteColor))
 }
